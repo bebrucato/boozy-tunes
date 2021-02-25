f17e3f347d2190423c1bcb0c83bf49c3eb449990
B Brucato <bebrucato@gmail.com>
2021-02-25 12:27:05 -0800

    fixed drop down

--- style/script.js
+++ style/script.js
@@ -1,18 +1,20 @@
-var img = $(".blinking-fire");
-var count=0
-function flames(){
-    count++
-    if (count%2===0) {
-    $(".blinking-fire").animate({left:'20px', opacity: '0.4'},"fast");
-}else {
-    $(".blinking-fire").animate({left:'20px', opacity: '1.0'},"fast");
-}
-}
-
-setInterval(flames,1000)
-console.log("hello")
-
-
-$(".dropdown-toggle").on("click",$(".menu1").show());
-console.log("hi")
+$(document).ready(function (){ 
+    var img = $(".blinking-fire");
+    var count=0
+    function flames(){
+        count++
+        if (count%2===0) {
+        $(".blinking-fire").animate({left:'20px', opacity: '0.4'},"fast");
+    }else {
+        $(".blinking-fire").animate({left:'20px', opacity: '1.0'},"fast");
+    }
+    }
+    
+    setInterval(flames,1000)
+    console.log("hello")
+    
+    
+  
+    
+})
 
